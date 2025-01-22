74ac35d94acd363fdd5ce5131761ae22609c0634
Pranto Deb <rajprit0m@outlook.com>
2025-01-22 22:02:41 +0600

    exlintrc and prettier setup

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,26 +1,33 @@
-const eslintPlugin = require("@typescript-eslint/eslint-plugin");
-const eslintParser = require("@typescript-eslint/parser");
+const eslintPlugin = require('@typescript-eslint/eslint-plugin');
+const eslintParser = require('@typescript-eslint/parser');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = [
   {
-    ignores: ["node_modules/", "dist/", ".env"],
+    ignores: ['node_modules/', 'dist/', '.env'],
   },
+  eslintPluginPrettierRecommended,
   {
-    files: ["src/**/*.ts"],
+    files: ['src/**/*.ts'],
     languageOptions: {
       parser: eslintParser,
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+      },
     },
     plugins: {
-      "@typescript-eslint": eslintPlugin,
+      '@typescript-eslint': eslintPlugin,
     },
     rules: {
-      "no-console": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "no-var": "error",
-      "prefer-const": "warn",
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-unused-expressions': 'error',
+      'no-undef': 'warn',
     },
   },
 ];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 // const port = 3000;
@@ -8,10 +8,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req: Request, res: Response) => {
+  const a = 8;
 
-app.get("/", (req: Request, res: Response) => {
-    const a = 8;
   res.send(a);
 });
 
-export default app
+export default app;
