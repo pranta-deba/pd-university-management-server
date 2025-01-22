b1e3ba13f1bb6b1ee965116dc6629cd58a4d2735
Pranto Deb <rajprit0m@outlook.com>
2025-01-23 01:06:21 +0600

    [Model, Route, Controller, Services] : create a student

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model } from 'mongoose';
 import {
   Guardian,
   LocalGuardian,
@@ -51,4 +51,4 @@ const studentSchema = new Schema<Student>({
   },
 });
 
-const Student = model<Student>('Student', studentSchema);
+export const StudentModel = model<Student>('Student', studentSchema);
