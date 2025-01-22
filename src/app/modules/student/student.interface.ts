20dae3ab987c1bcf4da2b785803e94ca6dfa6fcc
Pranto Deb <rajprit0m@outlook.com>
2025-01-22 23:50:56 +0600

    [interface] create student interface

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,27 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+export type Student = {
+  id: string;
+  name: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+  gender: 'male' | 'female' | 'other';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+};
