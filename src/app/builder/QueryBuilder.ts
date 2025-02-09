5ab162bc49677064dc2954fcccb547760d768673
Pranto Deb <rajprit0m@outlook.com>
2025-02-09 22:58:30 +0600

    Practice Task Start

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -37,14 +37,15 @@ class QueryBuilder<T> {
   }
 
   sort() {
-    const sort = this?.query?.sort || '-createdAt';
+    const sort =
+      (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
     this.modelQuery = this.modelQuery.sort(sort as string);
     return this;
   }
 
   paginate() {
     const page = Number(this?.query?.page) || 1;
-    const limit = Number(this?.query?.limit) || 1;
+    const limit = Number(this?.query?.limit) || 10;
     const skip = (page - 1) * limit;
     this.modelQuery = this.modelQuery.skip(skip).limit(limit);
     return this;
