65ac7ffa4907cf05b58076709b96e30189e5b7a5
Pranto Deb <rajprit0m@outlook.com>
2025-02-09 22:08:23 +0600

    Refactor your code and build a Query Builder 1

---
+++ src/app/builder/QueryBuilder.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Query } from 'mongoose';
+
+/* eslint-disable prettier/prettier */
+class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+
+  search(searchableFields: string[]) {}
+}
