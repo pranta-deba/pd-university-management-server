b95a7d7032a758ee66243bd2172a33c5373ff71e
Pranto Deb <rajprit0m@outlook.com>
2025-02-20 23:22:21 +0600

    semester registration : service [get all]

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -4,6 +4,7 @@ import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import AppError from '../../errors/AppError';
 import { SemesterRegistration } from './semesterRegistration.model';
+import QueryBuilder from '../../builder/QueryBuilder';
 
 /* eslint-disable prettier/prettier */
 const createSemesterRegistrationIntoDB = async (
@@ -25,7 +26,6 @@ const createSemesterRegistrationIntoDB = async (
   if (isSemesterRegistrationExists) {
     throw new AppError(status.CONFLICT, 'This semester is already registered!');
   }
-  
 
   const result = await SemesterRegistration.create(payload);
   return result;
@@ -33,7 +33,19 @@ const createSemesterRegistrationIntoDB = async (
 
 const getAllSemesterRegistrationsFromDB = async (
   query: Record<string, unknown>,
-) => {};
+) => {
+  const semesterRegistrationQuery = new QueryBuilder(
+    SemesterRegistration.find().populate('academicSemester'),
+    query,
+  )
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await semesterRegistrationQuery.modelQuery;
+  return result;
+};
 
 const getSingleSemesterRegistrationsFromDB = async (id: string) => {};
 
