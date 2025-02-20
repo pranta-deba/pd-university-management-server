e1ba738f9e10c8f91f24497d8e5cfb4378753a3f
Pranto Deb <rajprit0m@outlook.com>
2025-02-21 02:16:58 +0600

    [Offered Course] : [service] create

---
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { TOfferedCourse } from './OfferedCourse.interface';
+import { OfferedCourse } from './OfferedCourse.model';
+
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  const {
+    semesterRegistration,
+    academicFaculty,
+    academicDepartment,
+    course,
+    section,
+    faculty,
+    days,
+    startTime,
+    endTime,
+  } = payload;
+
+  const result = await OfferedCourse.create(payload);
+  return result;
+};
