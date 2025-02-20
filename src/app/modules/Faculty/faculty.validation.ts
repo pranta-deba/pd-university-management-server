65433485546cdfd56c92cbf5a0f6238ee6ecfb29
Pranto Deb <rajprit0m@outlook.com>
2025-02-20 21:21:06 +0600

    Create CourseFaculty interface , model , validation , controller and test with postman

--- src/app/modules/Faculty/faculty.validation.ts
+++ src/app/modules/Faculty/faculty.validation.ts
@@ -50,7 +50,7 @@ export const updateFacultyValidationSchema = z.object({
       email: z.string().email().optional(),
       contactNo: z.string().optional(),
       emergencyContactNo: z.string().optional(),
-      bloogGroup: z.enum([...BloodGroup] as [string, ...string[]]).optional(),
+      bloodGroup: z.enum([...BloodGroup] as [string, ...string[]]).optional(),
       presentAddress: z.string().optional(),
       permanentAddress: z.string().optional(),
       profileImg: z.string().optional(),
