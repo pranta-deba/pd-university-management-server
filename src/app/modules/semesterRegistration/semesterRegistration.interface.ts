d7814f19931175a91be8123d23b3608db38e6d8a
Pranto Deb <rajprit0m@outlook.com>
2025-02-20 22:19:03 +0600

    Semester Registration interface

---
+++ src/app/modules/semesterRegistration/semesterRegistration.interface.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Types } from 'mongoose';
+
+export type TSemesterRegistration = {
+  academicSemester: Types.ObjectId;
+  status: 'UPCOMING' | 'ONGOING' | 'ENDED';
+  startDate: Date;
+  endDate: Date;
+  minCredit: number;
+  maxCredit: number;
+};
