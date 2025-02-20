1fdb2ad17b2f17433be33899234bf654ebad0579
Pranto Deb <rajprit0m@outlook.com>
2025-02-20 22:27:22 +0600

    Semester Registration service create register

---
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { TSemesterRegistration } from './semesterRegistration.interface';
+
+/* eslint-disable prettier/prettier */
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration,
+) => {};
+
+
+
+export const SemesterRegistrationService = {
+  createSemesterRegistrationIntoDB,
+};
