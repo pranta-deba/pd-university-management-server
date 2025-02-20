40ec53154c7ddb99bf6cfa98b88cbcca4f3f1bc6
Pranto Deb <rajprit0m@outlook.com>
2025-02-20 23:14:49 +0600

    semester registration : create semester controller result send

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -3,6 +3,7 @@ import status from 'http-status';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import AppError from '../../errors/AppError';
+import { SemesterRegistration } from './semesterRegistration.model';
 
 /* eslint-disable prettier/prettier */
 const createSemesterRegistrationIntoDB = async (
@@ -16,6 +17,18 @@ const createSemesterRegistrationIntoDB = async (
   if (!isAcademicSemesterExists) {
     throw new AppError(status.NOT_FOUND, 'This academic semester not found !');
   }
+
+  // check if the semester is already registered!
+  const isSemesterRegistrationExists = await SemesterRegistration.findOne({
+    academicSemester,
+  });
+  if (isSemesterRegistrationExists) {
+    throw new AppError(status.CONFLICT, 'This semester is already registered!');
+  }
+  
+
+  const result = await SemesterRegistration.create(payload);
+  return result;
 };
 
 const getAllSemesterRegistrationsFromDB = async (
