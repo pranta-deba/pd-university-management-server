02f63cb52732324c379f5f3eb6d2289338395ba0
Pranto Deb <rajprit0m@outlook.com>
2025-02-20 22:20:24 +0600

    Semester Registration validation and constant variable

---
+++ src/app/modules/semesterRegistration/semesterRegistration.constant.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export const SemesterRegistrationStatus = ['UPCOMING', 'ONGOING', 'ENDED'];
+
+export const RegistrationStatus = {
+  UPCOMING: 'UPCOMING',
+  ONGOING: 'ONGOING',
+  ENDED: 'ENDED',
+} as const;
