b87dd9245c4a8fefb4a17c0ebec58a903de67eb0
Pranto Deb <rajprit0m@outlook.com>
2025-02-20 22:36:01 +0600

    Semester register routes

---
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -0,0 +1,43 @@
+/* eslint-disable prettier/prettier */
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { SemesterRegistrationController } from './semesterRegistration.controller';
+import { SemesterRegistrationValidations } from './semesterRegistration.validation';
+
+/* eslint-disable prettier/prettier */
+const router = express.Router();
+
+router.post(
+  '/create-semester-registration',
+  validateRequest(
+    SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
+  ),
+  SemesterRegistrationController.createSemesterRegistration,
+);
+
+router.get(
+  '/:id',
+  SemesterRegistrationController.getSingleSemesterRegistration,
+);
+
+router.patch(
+  '/:id',
+  validateRequest(
+    SemesterRegistrationValidations.updateSemesterRegistrationValidationSchema,
+  ),
+  SemesterRegistrationController.updateSemesterRegistration,
+);
+
+router.get(
+  '/:id',
+  SemesterRegistrationController.getSingleSemesterRegistration,
+);
+
+router.delete(
+  '/:id',
+  SemesterRegistrationController.deleteSemesterRegistration,
+);
+
+router.get('/', SemesterRegistrationController.getAllSemesterRegistrations);
+
+export const semesterRegistrationRoutes = router;
