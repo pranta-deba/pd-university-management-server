2a7e6d6d83437dbc49f18ed75005dcbcfaa91501
Pranto Deb <rajprit0m@outlook.com>
2025-02-21 16:20:02 +0600

    [Auth] : [middleware] checking if the given token is valid

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import status from 'http-status';
+import jwt from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +13,20 @@ const auth = () => {
     if (!token) {
       throw new AppError(status.UNAUTHORIZED, 'You are not authorized!');
     }
+
+    // checking if the given token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        // err
+        if (err) {
+          throw new AppError(status.UNAUTHORIZED, 'You are not authorized!');
+        }
+        // decoded
+        console.log(decoded);
+      },
+    );
 
     next();
   });
