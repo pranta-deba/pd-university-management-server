7e21fe29e5ebdcfee57fd80e24a04fcc130edd7c
Pranto Deb <rajprit0m@outlook.com>
2025-02-21 13:27:36 +0600

    [Auth] : [service] [login user] checking if the password is correct

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,24 +2,30 @@ import status from 'http-status';
 import { TLoginUser } from './auth.interface';
 import { User } from '../user/user.model';
 import AppError from '../../errors/AppError';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
   // checking if the user is exist
-  const isUserExists = await User.findOne({ id: payload.id });
-  if (!isUserExists) {
+  const user = await User.isUserExistsByCustomId(payload.id);
+  if (!user) {
     throw new AppError(status.NOT_FOUND, 'This user is not found !');
   }
 
   // checking if the user is already deleted
-  const isDeleted = isUserExists?.isDeleted;
+  const isDeleted = user?.isDeleted;
   if (isDeleted) {
     throw new AppError(status.FORBIDDEN, 'This user is deleted !');
   }
 
   // checking if the user is blocked
-  const userStatus = isUserExists?.status;
+  const userStatus = user?.status;
   if (userStatus === 'blocked') {
-    throw new AppError(status.FORBIDDEN, 'This user is blocked ! !');
+    throw new AppError(status.FORBIDDEN, 'This user is blocked !');
+  }
+
+  //checking if the password is correct
+  if (!(await User.isPasswordMatched(payload?.password, user?.password))) {
+    throw new AppError(status.FORBIDDEN, 'Password do not matched');
   }
 
   return {};
