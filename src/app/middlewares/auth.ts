da118438a850853bf7e01cb33b002867d8db7e43
Pranto Deb <rajprit0m@outlook.com>
2025-02-21 15:29:25 +0600

    [Auth] : [middleware] checking if the token is missing

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,9 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import status from 'http-status';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.headers.authorization);
+    const token = req.headers.authorization;
+
+    // checking if the token is missing
+    if (!token) {
+      throw new AppError(status.UNAUTHORIZED, 'You are not authorized!');
+    }
+
     next();
   });
 };
