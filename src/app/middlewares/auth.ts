b9426b04bf34b7e8962fa9b180387d9bc061c4db
Pranto Deb <rajprit0m@outlook.com>
2025-02-21 15:27:01 +0600

    [Auth] : [middleware]"

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,14 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import { AnyZodObject } from 'zod';
 import catchAsync from '../utils/catchAsync';
 
-const validateRequest = (schema: AnyZodObject) => {
+const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    await schema.parseAsync({
-      body: req.body,
-    });
+    console.log(req.headers.authorization);
     next();
   });
 };
 
-export default validateRequest;
+export default auth;
