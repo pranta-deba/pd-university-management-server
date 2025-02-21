6e69922a15cfdc245eff9a809d0c780e083070e7
Pranto Deb <rajprit0m@outlook.com>
2025-02-21 12:57:19 +0600

    [Auth] : [service] [login user] checking if the user is exist

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -1,10 +1,23 @@
+import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
+import { AuthServices } from './auth.service';
+import sendResponse from '../../utils/sendResponse';
+import status from 'http-status';
 
-const loginUser = catchAsync(async (req, res) => {});
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+  const result = await AuthServices.loginUser(req.body);
 
-const changePassword = catchAsync(async (req, res) => {});
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'User is logged in successfully!',
+    data: result,
+  });
+});
 
-const refreshToken = catchAsync(async (req, res) => {});
+const changePassword = catchAsync(async (req: Request, res: Response) => {});
+
+const refreshToken = catchAsync(async (req: Request, res: Response) => {});
 
 export const AuthControllers = {
   loginUser,
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,4 +1,18 @@
-const loginUser = async () => {};
+import status from 'http-status';
+import { TLoginUser } from './auth.interface';
+import { User } from '../user/user.model';
+import AppError from '../../errors/AppError';
+
+const loginUser = async (payload: TLoginUser) => {
+  // checking if the user is exist
+  const isUserExists = await User.findOne({ id: payload.id });
+
+  if (!isUserExists) {
+    throw new AppError(status.NOT_FOUND, 'This user is not found !');
+  }
+
+  return {};
+};
 
 const changePassword = async () => {};
 
