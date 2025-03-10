c7fe07f1f25b0c4fa88be73eb7a6226b9dd8c0e6
Pranto Deb <rajprit0m@outlook.com>
2025-03-10 21:29:47 +0600

    [Enrolled : route templete created

---
+++ src/app/modules/EnrolledCourse/enrolledCourse.route.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import auth from '../../middlewares/auth';
+import { EnrolledCourseValidations } from './enrolledCourse.validation';
+import { EnrolledCourseControllers } from './enrolledCourse.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-enrolled-course',
+  auth('student'),
+  validateRequest(
+    EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
+  ),
+  EnrolledCourseControllers.createEnrolledCourse,
+);
+
+router.patch(
+  '/update-enrolled-course-marks',
+  auth('faculty'),
+  validateRequest(
+    EnrolledCourseValidations.updateEnrolledCourseMarksValidationZodSchema,
+  ),
+  EnrolledCourseControllers.updateEnrolledCourseMarks,
+);
+
+export const EnrolledCourseRoutes = router;
