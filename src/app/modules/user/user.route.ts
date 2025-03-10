ecb923a1ba25d18fecf0a88206fabf8d74c9a262
Pranto Deb <rajprit0m@outlook.com>
2025-03-10 20:59:02 +0600

    [File] : faculty and admin

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -26,6 +26,11 @@ router.post(
 router.post(
   '/create-faculty',
   auth(USER_ROLE.admin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
   validateRequest(createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
@@ -33,6 +38,11 @@ router.post(
 router.post(
   '/create-admin',
   // auth(USER_ROLE.admin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
   validateRequest(createAdminValidationSchema),
   UserControllers.createAdmin,
 );
