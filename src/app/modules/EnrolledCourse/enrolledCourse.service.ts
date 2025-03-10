6b8015fc112d8a6a772246b31564c15fa7db8919
Pranto Deb <rajprit0m@outlook.com>
2025-03-10 22:04:18 +0600

    Enroll Course] : [Services] : [Create Enroll] - Check if the student is already enrolled

--- src/app/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -2,6 +2,8 @@ import status from 'http-status';
 import { OfferedCourse } from '../OfferedCourse/OfferedCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import AppError from '../../errors/AppError';
+import { Student } from '../student/student.model';
+import EnrolledCourse from './enrolledCourse.model';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
@@ -16,6 +18,20 @@ const createEnrolledCourseIntoDB = async (
   }
 
   // Step2: Check if the student is already enrolled
+  const student = await Student.findOne({ id: userId }, { _id: 1 });
+  if (!student) {
+    throw new AppError(status.NOT_FOUND, 'Student not found !');
+  }
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+    offeredCourse,
+    student: student._id,
+  });
+
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(status.CONFLICT, 'Student is already enrolled !');
+  }
+
   // Step3: Check if the max credits exceed
   // Step4: Create an enrolled course
 };
