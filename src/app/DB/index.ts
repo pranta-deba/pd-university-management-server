b0945f83a4d861cbc6016c4eb3dbc7fbdf390d61
Pranto Deb <rajprit0m@outlook.com>
2025-03-11 23:30:06 +0600

    [Super Admin] - when database is connected, we will check is there any user who is super admin

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -1,5 +1,6 @@
 import config from '../config';
 import { USER_ROLE } from '../modules/user/user.constant';
+import { User } from '../modules/user/user.model';
 
 const superUser = {
   id: '0001',
@@ -11,4 +12,13 @@ const superUser = {
   isDeleted: false,
 };
 
-const seedSuperAdmin = () => {};
+//when database is connected, we will check is there any user who is super admin
+const seedSuperAdmin = async () => {
+  const isSuperAdminExits = await User.findOne({ role: USER_ROLE.superAdmin });
+
+  if (!isSuperAdminExits) {
+    await User.create(superUser);
+  }
+};
+
+export default seedSuperAdmin;
