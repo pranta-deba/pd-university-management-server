c29db2208d82b9c23fbedd385d1cc7931eca85d5
Pranto Deb <rajprit0m@outlook.com>
2025-03-11 23:27:19 +0600

    [Super Admin] create info

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -1,3 +1,14 @@
-const seedSuperAdmin = () => {
-    
+import config from '../config';
+import { USER_ROLE } from '../modules/user/user.constant';
+
+const superUser = {
+  id: '0001',
+  email: 'superAdmin@gmail.com',
+  password: config.super_admin_password,
+  needsPasswordChange: false,
+  role: USER_ROLE.superAdmin,
+  status: 'in-progress',
+  isDeleted: false,
 };
+
+const seedSuperAdmin = () => {};
