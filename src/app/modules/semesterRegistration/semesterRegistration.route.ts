49e3ac7a7e36c65e5990bf46ba9e4a6524773d3c
Pranto Deb <rajprit0m@outlook.com>
2025-03-26 18:32:18 +0600

    [Semester Registration]:[Route]: set auth gaurd

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { SemesterRegistrationController } from './semesterRegistration.controller';
 import { SemesterRegistrationValidations } from './semesterRegistration.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-semester-registration',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
