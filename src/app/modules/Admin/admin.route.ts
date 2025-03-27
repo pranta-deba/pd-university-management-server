2e5d80603166ebdf0d22c07d1c8eb21f526dd07b
Pranto Deb <rajprit0m@outlook.com>
2025-03-27 13:51:53 +0600

    [Updated] : [Route Auth Guard] : [Admin]

--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -7,9 +7,17 @@ import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', auth(USER_ROLE.superAdmin), AdminControllers.getAllAdmins);
+router.get(
+  '/',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  AdminControllers.getAllAdmins,
+);
 
-router.get('/:id', auth(USER_ROLE.superAdmin), AdminControllers.getSingleAdmin);
+router.get(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  AdminControllers.getSingleAdmin,
+);
 
 router.patch(
   '/:id',
